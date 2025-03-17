b1d5dda97c4dfcf74b47a3223b8b0205d82bf0ad
Quentin Flachaire <quentin.flachaire@qarnot-computing.com>
2025-03-17 17:55:49 +0100

    openfoam download result + blacklist and whitelist

--- openfoam/run.py
+++ openfoam/run.py
@@ -36,7 +36,41 @@ task.constants['DOCKER_TAG'] = OPENFOAM_VERSION
 # task.constants['OPENFOAM_INPUT_DIRECTORY_NAME'] = 'motorbike'
 
 # Optional, define interval time in seconds when your simulation will be saved to your bucket.
-# task.snapshot(600)
+task.snapshot(900)
+
+# Optionnal, blacklist processor directories with regex
+task.snapshot_blacklist = r"processor\d+" # Set snapshots blacklist
+task.results_blacklist = r"processor\d+" # Set results blacklist
+
+# Optionnal, whitelist only log. files with regex
+# task.snapshot_whitelist = r"log\..*" # Set snapshots whitelist
+# task.results_whitelist = r"log\..*" # Set results whitelist
+
+# OnDemand setup
+# task.scheduling_type=OnDemandScheduling()
 
 # Submit the task
 task.submit()
+
+# # ---------- Optional ----------
+OUTPUT_DIR="motorbike-out"
+# The following will download result to the OUTPUT_DIR 
+# It will also print the state of the task to your console
+LAST_STATE = ''
+SSH_TUNNELING_DONE = False
+while not SSH_TUNNELING_DONE:
+    if task.state != LAST_STATE:
+        LAST_STATE = task.state
+        print(f"** {LAST_STATE}")
+
+    # Wait for the task to be FullyExecuting
+    if task.state == 'Success':
+        print(f"** {LAST_STATE}")
+        task.download_results(OUTPUT_DIR, True)
+        SSH_TUNNELING_DONE = True
+
+    # Display errors on failure
+    if task.state == 'Failure':
+        print(f"** Errors: {task.errors[0]}")
+        SSH_TUNNELING_DONE = True
+
