c310b0c071f41ce5c897c709d61e2fd26c099b11
chloe.pilon-vaillancourt <chloe.pilon-vaillancourt@qarnot-computing.com>
2025-11-13 12:48:15 +0100

    altair : add corrections

--- altair/run_altair.py
+++ altair/run_altair.py
@@ -5,7 +5,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# =============================== Setup Variables =============================== #
+# =============================== SETUP VARIABLES =============================== #
+
+# =============================== Mandatory Variables =============================== #
 
 CLIENT_TOKEN="MY_SECRET_TOKEN"                 # To retrieve on your HPC or Tasq account
 CLIENT_TOKEN=os.getenv("QARNOT_TOKEN")         # Using an env variable
@@ -14,14 +16,10 @@ PROFILE="YOUR_PROFILE"                         # Example : 'altair-hyperworks-qa
 NB_INSTANCES = 2                               # Number of instances in your cluster.
 ALTAIR_VERSION="2024.1"                        # Altair Hyperwork 2024.1 
                         
-DIR_TO_SYNC = 'altair_block_test'              # Exact name for your model's directory (for example "model_dir", if Radioss) or file with extension (for exemple "model.fem", if Optistruct)
+DIR_TO_SYNC = 'altair_block_test'              # Exact name for your model's directory containg your .rad (Radioss) or .fem (Optistruct) model
 INPUT_BUCKET_NAME =  f"{DIR_TO_SYNC}-in"    
 OUTPUT_BUCKET_NAME = f"{DIR_TO_SYNC}-out"
-OUTPUT_FILTER = r"(?i).*\.3ds$"                # Optional : Regex filter to select which outputfiles you want to keep. Here, an example with .h3d
-
 TASK_NAME = f"RUN test Altair - {DIR_TO_SYNC}" 
-USE_MAX_EXEC_TIME = "false"                    # Optional : Set to true to activate the configuration of maximum cluster execution time. 
-MAX_EXEC_TIME = "8h"                           # Optional : Maximum cluster execution time (ex: '8h', 'h' for hours or 'd' for days) if USE_MAX_EXEC_TIME is true" 
 
 INSTANCE_TYPE = 'Xeon'                         # Xeon is the default choice. Otherwise, put 'Epyc'.
 if INSTANCE_TYPE == 'Xeon':
@@ -33,16 +31,30 @@ elif INSTANCE_TYPE == 'EPYC':
 
 ALTAIR_CMD = f"optistruct -np {SETUP_CLUSTER_NB_SLOTS}*{NB_INSTANCES} block.fem" # Your Altair Hyperworks CMD, depending on your solver
 
+# =============================== Optional Variables =============================== #
+
+#OUTPUT_FILTER = r"(?i).*\.h3d$"                # Optional : Regex filter to select which outputfiles you want to keep. Here, an example with .h3d
+#SYNC_FILTER = OUTPUT_FILTER                    # Optional : Regex filter to select which files are copied during your snapshots
+
+USE_MAX_EXEC_TIME = "false"                    # Optional : Set to true to activate the configuration of maximum cluster execution time. 
+MAX_EXEC_TIME = "8h"                           # Optional : Maximum cluster execution time (ex: '8h', 'h' for hours or 'd' for days) if USE_MAX_EXEC_TIME is true" 
+
 POST_PROCESSING_CMD = ""                       # Optional : Post processing command, ran after simulation if not empty.
                                                # Use '$optistruct --help' or '$radioss --help' on ssh to understand all the possible flags, or contact our team to help you optimize your case.
 
-# =============================== Lauching a Task =============================== #
+
+
+
+# =============================== TASK CONFIGURATION =============================== #
+
+# =============================== Mandatory Configuration =============================== #
 
 # Create a connection, from which all other objects will be derived
 conn = qarnot.connection.Connection(client_token=CLIENT_TOKEN)
 
 # Print available profiles with you account
-print([profile for profile in conn.profiles_names() if 'altair' in profile])
+avail_profile = [profile for profile in conn.profiles_names() if 'altair' in profile]
+print(f'Available profiles for your account : {avail_profile}')
 
 # Create task
 task = conn.create_task(TASK_NAME, PROFILE, NB_INSTANCES)
@@ -55,26 +67,12 @@ task.resources.append(input_bucket)
 # Create a result bucket and attach it to the task
 output_bucket = conn.create_bucket(OUTPUT_BUCKET_NAME)
 task.results = output_bucket
-task.results_whitelist  = OUTPUT_FILTER
 
-# Required settings
 ## Historically, at Qarnot, the Altair Hyperworks Suite was named "Altair Mechanical" at Qarnot. We kept the variable value, but don't worry - It is Altair Hyperworks!
 task.constants["ALTAIR_MECHA_CMD"] = ALTAIR_CMD
 task.constants['DOCKER_TAG'] = ALTAIR_VERSION
 task.constants["SETUP_CLUSTER_NB_SLOTS"] = SETUP_CLUSTER_NB_SLOTS
 task.hardware_constraints = [qarnot.hardware_constraint.SpecificHardware(instance_type)]
-
-# Optional settings
-task.constants['POST_PROCESSING_CMD'] = POST_PROCESSING_CMD
-task.constants['USE_SIMULATION_MAXIMUM_EXECUTION_TIME'] = USE_MAX_EXEC_TIME
-task.constants['SIMULATION_MAXIMUM_EXECUTION_TIME'] = MAX_EXEC_TIME
-
-# Settings to copy from simulation directory (/share) to bucket linked directory (/job).
-##  /job  is the dir where buckets are downloaded at start and uploaded to your bucket by the snapshots.
-## /share is the dir where the simulation is executing. Fastest disk and shared directories between nodes.
-task.constants['LOCAL_FILES_COPY_FEATURE'] = "true"       # Set to true to upload periodically from the /share folder
-task.constants['LOCAL_FILES_COPY_INTERVAL_SEC'] = "1800"  # Set the upload interval in seconds
-task.constants['LOCAL_FILES_COPY_REGEX'] = ""             # Filters the files to upload, leave empty to upload everything
 
 # Scheduling type
 task.scheduling_type=OnDemandScheduling()
@@ -82,5 +80,25 @@ task.scheduling_type=OnDemandScheduling()
 # task.scheduling_type=ReservedScheduling()               # If your company has reserved nodes
 # task.targeted_reserved_machine_key = instance_type      # Uncomment if your company has reserved nodes
 
-# You are ready to submit your task!
+
+# =============================== Optional Configuration =============================== #
+
+#task.results_whitelist  = OUTPUT_FILTER
+#task.snapshots_whitelist  = SYNC_FILTER
+
+#task.constants['POST_PROCESSING_CMD'] = POST_PROCESSING_CMD
+#task.constants['USE_SIMULATION_MAXIMUM_EXECUTION_TIME'] = USE_MAX_EXEC_TIME
+#task.constants['SIMULATION_MAXIMUM_EXECUTION_TIME'] = MAX_EXEC_TIME
+
+# Settings to copy from simulation directory (/share) to bucket linked directory (/job).
+##  /job  is the dir where buckets are downloaded at start and uploaded to your bucket by the snapshots.
+## /share is the dir where the simulation is executing. Fastest disk and shared directories between nodes.
+
+#task.constants['LOCAL_FILES_COPY_FEATURE'] = "true"       # Set to true to upload periodically from the /share folder
+#task.constants['LOCAL_FILES_COPY_INTERVAL_SEC'] = "1800"  # Set the upload interval in seconds
+#task.constants['LOCAL_FILES_COPY_REGEX'] = ""             # Filters the files to upload, leave empty to upload everything
+
+
+# =============================== LAUNCH YOUR TASK ! =============================== #
+
 task.submit()
