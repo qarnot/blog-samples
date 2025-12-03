2930df414256532e986495e0b7453cf07a37f7d2
chloe.pilon-vaillancourt <chloe.pilon-vaillancourt@qarnot-computing.com>
2025-12-03 20:28:55 +0100

    add V1-TBC openfoamfoundation scripts

---
+++ openfoam-foundation/run_openfoamfoundation.py
@@ -0,0 +1,59 @@
+"""
+Script to launch a simple Openfoam task on Qarnot's platform
+"""
+
+import qarnot
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# =============================== SETUP VARIABLES =============================== #
+
+# =============================== Mandatory Variables =============================== #
+
+CLIENT_TOKEN=os.getenv("QARNOT_TOKEN")         # If your token is in a .env. You can also execute, in your terminal, 'export QARNOT_TOKEN='your_token''.
+PROFILE="openfoam-foundation"                        
+
+NB_INSTANCES = 2                               # Number of instances in your cluster.
+
+PATH_TO_DIR = '/home/chloe.pilon/Documents/BENCH/Openfoam/MOTORBIKE-11'     # Path to your model's directory
+DIR_TO_SYNC = 'MOTORBIKE-11'                    # Name for your model's directory
+INPUT_BUCKET_NAME =  f"{DIR_TO_SYNC}-in"    
+OUTPUT_BUCKET_NAME = f"{DIR_TO_SYNC}-out"
+TASK_NAME = f"RUN test OpenfoamFoundation - {DIR_TO_SYNC}" 
+
+OPENFOAMF_CMD = "Allrun"          
+# =============================== TASK CONFIGURATION =============================== #
+
+# Create a connection, from which all other objects will be derived
+conn = qarnot.connection.Connection(client_token=CLIENT_TOKEN)
+
+# Create task
+task = conn.create_task(TASK_NAME, PROFILE, NB_INSTANCES)
+
+# Create the input bucket and synchronize with a local folder
+input_bucket = conn.create_bucket(INPUT_BUCKET_NAME)
+input_bucket.sync_directory(PATH_TO_DIR)
+task.resources.append(input_bucket)
+
+# Create a result bucket and attach it to the task
+output_bucket = conn.create_bucket(OUTPUT_BUCKET_NAME)
+task.results = output_bucket
+
+# Specify Run script CMD, version, number of cores per node, etc. 
+task.constants['RUN_SCRIPT'] = "Allrun"
+task.constants['OPENFOAM_INPUT_DIRECTORY_NAME'] = f"{DIR_TO_SYNC}-in"
+
+# Submitting task
+print('Submitting and running task on Qarnot. You can go to the online platform for more interactive task monitoring. Otherwise, the results will be downloaded automatically once the task reaches Success.')
+task.submit()
+
+# Download results when "Success" state is reached
+SUCCESS = False
+while not SUCCESS:
+    # Wait for the task to be FullyExecuting
+    if task.state == 'Success':
+      print('Task reached Success. Downloading Results.')
+      task.download_results(OUTPUT_BUCKET_NAME, True)
+      SUCCESS = True
